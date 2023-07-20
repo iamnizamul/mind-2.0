868848b27eddffedcf182aa3dd64caf4cbe1225c
iamnizamul <itsmenizamul>
2023-07-20 20:48:53 +0530

    Added Create Note Button

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -3,8 +3,22 @@
     <router-link :to="'/createnote'">
     <h1 class="header">Mind2.0</h1>
     </router-link>
+    <router-link :to="'/createnote'">
+    <base-button v-if="path !== '/createnote'" class="button">Create note</base-button>
+    </router-link>
   </header>
 </template>
+
+<script setup>
+import BaseButton from '../ui/BaseButton.vue';
+import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+
+const route = useRoute();
+const path = computed(() => route.path)
+console.log(path);
+
+</script>
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Lobster+Two:ital,wght@1,700&display=swap");
@@ -22,5 +36,10 @@ header {
   background-color: #5f3dc4;
   display: flex;
   align-items: center;
+  justify-content: space-between;
+}
+
+.button {
+  margin-right: 4rem;
 }
 </style>
